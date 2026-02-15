321c10b3a72494184d531236525b9ecfc4956d88
Girish Vλas <vgd.vyas17@gmail.com>
2026-02-15 17:01:10 +0530

    Add token logic

--- embeddings.py
+++ embeddings.py
@@ -1,3 +1,4 @@
+import re
 
 if __name__ == "__main__":
 
@@ -6,3 +7,19 @@ if __name__ == "__main__":
 
     print(f"No. of characters: {len(raw_text)}")
     print(f"Head: {raw_text[:99]}...")
+
+    # Match punctuation, whitespace
+    pttrn = r"[\W\s\n]+"
+
+    # Split at above pattern
+    # Remove last empty string: res.count("")
+    res = re.split(pttrn, raw_text)[:-1]
+
+    # Unnecessary
+    # res = [x.strip() for x in res if re.match("\w+", x)]
+
+    print(res)
+
+    tokens = {idx: val for idx, val in enumerate(set(res))}
+
+    print(tokens)
