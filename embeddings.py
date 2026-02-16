6e4ef8364f39b06d7a0fb9b738c71e7306183361
Girish Vλas <vgd.vyas17@gmail.com>
2026-02-17 00:09:34 +0530

    Switched tokens from simple regex to tiktoken

--- embeddings.py
+++ embeddings.py
@@ -1,61 +1,39 @@
-import re
-
-
-class WordTokens:
-
-    def __init__(self, txt):
-
-        res = self._process(txt)
-        res += ["<unk>", "<end>"]
-
-        self.tokens = {val: idx for idx, val in enumerate(set(res))}
-
-        self.decoded = {idx: val for val, idx in self.tokens.items()}
-
-    def _process(self, txt):
-        pttrn = re.compile(r"[\W\s\n]+")
-        res = re.split(pttrn, txt)
-        return res
-
-    def _encode(self, txt):
-        res = self._process(txt)
-
-        unk = self.tokens["<unk>"]
-        print(unk)
-        return [self.tokens.get(word, unk) for word in res]
-
-    def decode(self, tokens):
-        return " ".join([self.decoded[token] for token in tokens])
-
-    def encode(self, *txts):
-        end = self.tokens["<end>"]
-
-        res = []
-
-        for txt in txts:
-            res += self._encode(txt)
-            res.append(end)
-
-        return res
-
+from importlib.metadata import version
+import tiktoken
 
 if __name__ == "__main__":
+
+    print(f"""Verison: {version("tiktoken")}""")
+
+    tokenizer = tiktoken.get_encoding("gpt2")
 
     with open("./The_Verdict.txt", "r", encoding="utf-8") as f:
         book = f.read()
 
-    embeddings = WordTokens(book)
-
-    sent = embeddings.encode("silver trees are constantly destroyed, unacceptable!")
+    sent = tokenizer.encode(
+        "silver trees are constantly destroyed, unacceptable!",
+        allowed_special={"<|endoftext|>"},
+    )
 
     print(sent)
 
-    print(embeddings.decode(sent))
+    print(tokenizer.decode(sent))
 
-    par = embeddings.encode(
-        "silver trees are destroyed", "I believe constantly even, unacceptable"
+    par = tokenizer.encode(
+        """silver trees are destroyed.
+
+
+        A new line for the unruly.
+        I believe constantly even, unacceptable!""",
+        allowed_special={"<|endoftext|>"},
     )
 
     print(par)
 
-    print(embeddings.decode(par))
+    print(tokenizer.decode(par))
+
+    madeup = "sdffasdfasdf sdfasdfasdfsfasfsdfff asdfdfsfasdfasdfasfsfdasfsaf"
+
+    print(tokenizer.encode(madeup))
+    print(tokenizer.decode(tokenizer.encode(madeup)))
+    print(tokenizer.decode([21282]))
