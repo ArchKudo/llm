a212c7c7e4508971a1b755f449772afd37005504
Girish Vλas <vgd.vyas17@gmail.com>
2026-03-06 11:57:50 +0530

    Add a simple dataloader

--- embeddings.py
+++ embeddings.py
@@ -1,31 +1,63 @@
-from importlib.metadata import version
 import tiktoken
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+class GPTDataset(Dataset):
+
+    def __init__(self, text, tokenizer, max_len, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(text)
+
+        for i in range(0, len(token_ids) - max_len, stride):
+            input_chunk = token_ids[i : i + max_len]
+            target_chunk = token_ids[i + 1 : i + max_len + 1]
+
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+def create_dataloader(
+    text,
+    batch_size=4,
+    max_length=256,
+    stride=128,
+    drop_last=True,
+    shuffle=True,
+    num_workers=0,
+):
+
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDataset(text, tokenizer, max_length, stride)
+
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers,
+    )
+
 
 if __name__ == "__main__":
 
-    print(f"""Verison: {version("tiktoken")}""")
-
-    tokenizer = tiktoken.get_encoding("gpt2")
-
     with open("./The_Verdict.txt", "r", encoding="utf-8") as f:
-        book = f.read()
+        book = "".join(f.read().splitlines()[21:186])
 
-    encoded = tokenizer.encode(book)
+    print(book[:20])
+    dataloader = create_dataloader(
+        book, batch_size=1, max_length=4, stride=1, shuffle=False
+    )
 
-    print(len(encoded))
-
-    context_sz = 4
-
-    x = encoded[:context_sz]
-    y = encoded[1 : context_sz + 1]
-
-    print(x)
-    print(f"\t{y}")
-
-    nextword = [
-        (encoded[i : context_sz + i], encoded[context_sz + i])
-        for i in range(len(encoded) // context_sz)
-    ]
-
-    print(encoded[-80:])
-    print(nextword[-5:])
+    data_iter = iter(dataloader)
+    first_batch = next(data_iter)
+    print(first_batch)
+    print(next(data_iter))
